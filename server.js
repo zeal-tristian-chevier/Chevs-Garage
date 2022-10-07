19eb8b84391c0e30073ddc5fe51f06a4792f60ee
Tristian Chevier <80795010+tchevier@users.noreply.github.com>
2022-10-06 19:56:45 -0600

    commit

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ app.use(cors())
 
 app.use('/', projectRouter)
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static('client/build'));
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-//     })
-// }
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+}
 
 app.listen(port, () => console.log(`Listening on Port: ${port}`))
