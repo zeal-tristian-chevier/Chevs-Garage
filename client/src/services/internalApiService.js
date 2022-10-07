10645dc70aca0c4b3126e597f6f80b68d2f43be6
Tristian Chevier <80795010+tchevier@users.noreply.github.com>
2022-10-06 21:20:13 -0600

    make it better

--- client/src/services/internalApiService.js
+++ client/src/services/internalApiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-    baseURL: 'https://chevs-garag.herokuapp.com/'
+    baseURL: 'https://chevs-garage.herokuapp.com/'
 })
 export const getAllProjects = async() => {
     const res = await http.get('/')
