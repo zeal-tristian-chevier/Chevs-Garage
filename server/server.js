bc2bc2b1f84665a306074959f3704a7991907709
Tristian Chevier <80795010+tchevier@users.noreply.github.com>
2022-10-07 00:37:35 -0600

    make it better

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const {projectRouter} = require('./routes/project.routes')
+const port = process.env.PORT || 8000
+
+require('./config/mongoose.config')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+if (process.env.NODE_ENV === "production") {
+    console.log("PRODUCTION MODE ACTIVE");
+    //app.use('/', express.static("/client/build"));
+    app.use(express.static("./client/build"));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, './client/build','index.html'));
+    })
+  }
+app.use('/', projectRouter)
+
+app.listen(port, () => console.log(`Listening on Port: ${port}`))
