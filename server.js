f783170eb00f8d2b90d895ad05ce128b0ae3a40a
Tristian Chevier <80795010+tchevier@users.noreply.github.com>
2022-10-06 23:24:47 -0600

    make it better

--- server.js
+++ server.js
@@ -12,12 +12,13 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
-app.use('/', projectRouter)
-
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
+if (process.env.NODE_ENV === "production") {
+    console.log("PRODUCTION MODE ACTIVE");
+    //app.use('/', express.static("/client/build"));
+    app.use(express.static("./client/build"));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, './client/build','index.html'));
+    })
+  }
 
 app.listen(port, () => console.log(`Listening on Port: ${port}`))
