6d5f335d8dee07276dbd44904aa1bdbd1a039001
Tristian Chevier <80795010+tchevier@users.noreply.github.com>
2022-10-11 00:52:11 -0600

    yes

--- client/src/services/internalApiService.js
+++ client/src/services/internalApiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-    baseURL: `http://localhost:${process.env.PORT}`
+    baseURL: `https://chevs-garage.herokuapp.com:${process.env.PORT}`
 })
 export const getAllProjects = async() => {
     const res = await http.get('/api')
