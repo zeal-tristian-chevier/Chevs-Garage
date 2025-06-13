e1765644382a9d6325cf5634224d1b374af2c261
Tristian Chevier <tristian.chevier@zealitconsultants.com>
2025-06-13 10:39:46 -0500

    remove server

--- server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('Established a connection to the database'))
-    .catch(err => console.log('Something went wrong when connecting to the database ', err));
